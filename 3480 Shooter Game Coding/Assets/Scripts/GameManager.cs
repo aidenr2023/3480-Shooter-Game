1ce374415fa15e15c7bfcb18c7a6900a209b9a34
aidenr2023 <aiden.richmond56@gmail.com>
2023-11-03 19:56:51 -0400

    First Commit

---
+++ 3480 Shooter Game Coding/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public GameObject enemyOnePrefab;
+  public GameObject enemyTwoPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateEnemyOne()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+        Instantiate(enemyTwoPrefab, new Vector3(0, Random.Range(-3, 5), 0), Quaternion.identity);
+    }
+    
+    
+}
